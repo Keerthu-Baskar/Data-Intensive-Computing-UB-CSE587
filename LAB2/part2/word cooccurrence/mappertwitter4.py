e9c79c2c01b1555cc8e2db64e7ff46a140b1b57e
Keerthana Baskaran <48191481+Keerthu-Baskar@users.noreply.github.com>
2019-09-07 02:13:31 +0530

    Add files via upload

---
+++ LAB2/part2/word cooccurrence/mappertwitter4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+top_words = ['golf','course','club','play','trump','boys','team','tournament','game','check']
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        #print '%s\t%s' % (word, 1)
+        
+        for x in list(range(0,len(words)-1)):
+            for i in list(range(x,x+2)):
+                if words[x] != words[i] and words[x] in top_words:
+                    print '%s\t%s' % (str(words[x]+','+words[i]), 1)       
+        
